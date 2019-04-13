19f5ebbf2c551341091e35207d2cd52be519c0f5
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-04-13 15:20:24 +0900

    updated styling and comments

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const informationBox = document.querySelector('.information-box');
 const openButton = document.querySelector('.open-button');
 
 closeButton.addEventListener('click', () => {
-	informationBox.classList.add('animated', 'bounceOutRight');
+	informationBox.classList.add('animated', 'flipOutX');
 	setTimeout(function() {
-		informationBox.classList.remove('animated', 'bounceOutRight');
+		informationBox.classList.remove('animated', 'flipOutX');
 		informationBox.classList.add('hide-box');
 	}, 1000);
 });
 
 openButton.addEventListener('click', () => {
 	informationBox.classList.remove('hide-box');
-	informationBox.classList.add('animated', 'bounceInRight');
+	informationBox.classList.add('animated', 'flipInY');
 	setTimeout(function() {
-		informationBox.classList.remove('animated', 'bounceInRight');
+		informationBox.classList.remove('animated', 'flipInY');
 	}, 1000);
 });
